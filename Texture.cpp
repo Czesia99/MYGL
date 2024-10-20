8afd2733a50e47ea17d29adcab65e257eae99ef8
yann <yann.r.berger@gmail.com>
2024-10-20 05:25:02 +0200

    texture new functions fixed, correct format in texture storage

--- Texture.cpp
+++ Texture.cpp
@@ -15,22 +15,30 @@ namespace mygl
         if (data)
         {
             GLenum format;
-            if (nrComponents == 1)
+            GLenum storage_format;
+
+            if (nrComponents == 1) {
                 format = GL_RED;
-            else if (nrComponents == 3)
+                storage_format = GL_R8;
+            }
+            else if (nrComponents == 3) {
                 format = GL_RGB;
-            else if (nrComponents == 4)
+                storage_format = GL_RGB8;
+            }
+            else if (nrComponents == 4) {
                 format = GL_RGBA;
+                storage_format = GL_RGBA8;
+            }
 
             // glBindTexture(GL_TEXTURE_2D, texture);
             // glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-
             glTextureParameteri(texture, GL_TEXTURE_WRAP_S, wrap); //second argument was GL_REPEAT
             glTextureParameteri(texture, GL_TEXTURE_WRAP_T, wrap); //second argument was GL_REPEAT
             glTextureParameteri(texture, GL_TEXTURE_MIN_FILTER, min_filter);
             glTextureParameteri(texture, GL_TEXTURE_MAG_FILTER, mag_filter);
 
-            glTextureStorage2D(texture, 1, format, width, height);
+            glTextureStorage2D(texture, 1, storage_format, width, height);
+            glTextureSubImage2D(texture, 0, 0, 0, width, height, format, GL_UNSIGNED_BYTE, data);
             glGenerateTextureMipmap(texture);
 
             stbi_image_free(data);
