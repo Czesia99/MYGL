48ee76e2cf0f13b5e637c335dbd810273c1e8944
Yann <yann.r.berger@gmail.com>
2025-03-07 05:09:21 +0100

    chrono in toolbox

--- Toolbox.cpp
+++ Toolbox.cpp
@@ -1,6 +1,7 @@
 #include "Toolbox.hpp"
 #include "glm/glm.hpp"
-
+#include <chrono>
+#include <iostream>
 namespace mygl
 {
     void extractPlanesFromProjectionViewMatrix(const glm::mat4& m, glm::vec4 planes[6])
@@ -11,5 +12,17 @@ namespace mygl
         for (int i = 4; i--; ) { planes[3][i] = m[i][3] - m[i][1]; } // top
         for (int i = 4; i--; ) { planes[4][i] = m[i][3] + m[i][2]; } // near
         for (int i = 4; i--; ) { planes[5][i] = m[i][3] - m[i][2]; } // far
+    }
+
+    std::chrono::time_point<std::chrono::high_resolution_clock> start;
+    void startChrono()
+    {
+        start = std::chrono::high_resolution_clock::now();
+    }
+    void stopChrono()
+    {
+        auto stop = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+        std::cout << "Duration: " << duration << std::endl;
     }
 }
--- Toolbox.hpp
+++ Toolbox.hpp
@@ -2,8 +2,11 @@
 
 #include "glm/detail/type_vec4.hpp"
 #include "glm/detail/type_mat4x4.hpp"
+#include <chrono>
 
 namespace mygl
 {
     void extractPlanesFromProjectionViewMatrix(const glm::mat4& m, glm::vec4 planes[6]);
+    void startChrono();
+    void stopChrono();
 }
